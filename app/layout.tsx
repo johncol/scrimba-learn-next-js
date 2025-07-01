2dd05bf79e88fa98a2c5df290496b4415cb55b75
johncol <john.19col@gmail.com>
2025-07-01 15:27:02 -0700

    Update default font-size for all elements in the body

--- app/layout.tsx
+++ app/layout.tsx
@@ -3,15 +3,14 @@ import "./_styles/globals.css";
 import { MainNavigation } from "./_components/main-navigation/main-navigation";
 import styles from "./layout.module.css";
 import { albertSans, montserratAlternates } from "./_styles/fonts";
+import { PropsWithChildren } from "react";
 
 export const metadata: Metadata = {
   title: "PrintForge",
   description: "Following Scrimba course on Next.js",
 };
 
-export default function RootLayout({
-  children,
-}: Readonly<{ children: React.ReactNode }>) {
+export default function RootLayout({ children }: PropsWithChildren) {
   const fonts = `${albertSans.variable} ${montserratAlternates.variable}`;
   return (
     <html lang="en">
