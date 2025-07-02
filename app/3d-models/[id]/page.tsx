3994d31564d16d3f307427e608bec49b4fa92dfe
johncol <john.19col@gmail.com>
2025-07-01 20:58:34 -0700

    Update models and individual model page to access data through API functions

--- app/3d-models/[id]/page.tsx
+++ app/3d-models/[id]/page.tsx
@@ -1,5 +1,8 @@
+import { getModelById } from "@/app/_api/getModelById";
 import { DefaultContainer } from "@/app/_components/container/default-container";
 import { PageHeading } from "@/app/_components/page-heading/page-heading";
+import { Model } from "@/types/models";
+import { notFound } from "next/navigation";
 
 type ModelPageParams = {
   params: Promise<{
@@ -10,9 +13,25 @@ type ModelPageParams = {
 export default async function Page({ params }: ModelPageParams) {
   const { id } = await params;
 
+  try {
+    const model = await getModelById(id);
+
+    if (!model) {
+      return notFound();
+    }
+
+    return <ModelPage model={model} />;
+  } catch (error) {
+    console.warn(error);
+    return notFound();
+  }
+}
+
+const ModelPage = ({ model }: { model: Model }) => {
   return (
     <DefaultContainer>
-      <PageHeading>Model with ID {id}</PageHeading>
+      <PageHeading>{model.name}</PageHeading>
+      <p>{model.description}</p>
     </DefaultContainer>
   );
-}
+};
--- app/3d-models/page.tsx
+++ app/3d-models/page.tsx
@@ -1,13 +1,12 @@
-import type { Model } from "@/types/models";
-import ModelsJson from "@/data/models.json";
 import { DefaultContainer } from "../_components/container/default-container";
 import { ModelCard } from "../_components/model-card/model-card";
 
-import styles from "./page.module.css";
 import Link from "next/link";
+import { getAllModels } from "../_api/getAllModels";
+import styles from "./page.module.css";
 
 export default async function Page() {
-  const models = ModelsJson as Model[];
+  const models = await getAllModels();
 
   return (
     <DefaultContainer className={styles.modelsPage}>
