37e1e5e06f6e33b5260f9fb2db0dd9567f4946aa
johncol <john.19col@gmail.com>
2025-07-03 22:21:02 -0700

    Allow PageHeading to have large (default) and small variants

--- app/_components/page-heading/page-heading.tsx
+++ app/_components/page-heading/page-heading.tsx
@@ -2,8 +2,21 @@ import React from "react";
 
 import styles from "./page-heading.module.css";
 
-export const PageHeading: React.FC<React.HTMLProps<HTMLHeadingElement>> = (
-  props
-) => {
-  return <h1 {...props} className={styles.pageHeading} />;
+type Props = React.HTMLProps<HTMLHeadingElement> & {
+  fontSize?: "small" | "large";
+};
+
+export const PageHeading: React.FC<Props> = ({
+  fontSize = "large",
+  ...props
+}) => {
+  const className =
+    styles.pageHeading + " " + fontSizeStyles[fontSize] + " " + props.className;
+
+  return <h1 {...props} className={className} />;
+};
+
+const fontSizeStyles = {
+  small: styles.small,
+  large: styles.large,
 };
