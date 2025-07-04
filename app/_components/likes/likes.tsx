a1936ca57a015647b44a1844933d7353fafa8636
johncol <john.19col@gmail.com>
2025-07-03 22:21:24 -0700

    Allow Likes to receive a size

--- app/_components/likes/likes.tsx
+++ app/_components/likes/likes.tsx
@@ -2,14 +2,19 @@ import Image from "next/image";
 import styles from "./likes.module.css";
 
 import HeartIcon from "@/public/icons/Heart.svg";
+import { CSSProperties } from "react";
 
 type Props = {
   likes: number;
+  size?: string;
 };
 
-export const Likes = ({ likes }: Props) => {
+export const Likes = ({ likes, size }: Props) => {
   return (
-    <span className={styles.likes}>
+    <span
+      className={styles.likes}
+      style={{ "--font-size": size } as CSSProperties}
+    >
       <Image src={HeartIcon} alt="Heart" />
       <span>{likes}</span>
     </span>
