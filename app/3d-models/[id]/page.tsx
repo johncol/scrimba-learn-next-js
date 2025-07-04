5652b3d7b4214a45db7e7a85c30cf339444503c0
johncol <john.19col@gmail.com>
2025-07-03 22:22:31 -0700

    Build mobile and desktop versions of model page

--- app/3d-models/[id]/page.tsx
+++ app/3d-models/[id]/page.tsx
@@ -1,8 +1,13 @@
 import { getModelById } from "@/app/_api/getModelById";
 import { DefaultContainer } from "@/app/_components/container/default-container";
+import { Likes } from "@/app/_components/likes/likes";
 import { PageHeading } from "@/app/_components/page-heading/page-heading";
+import { Pill } from "@/app/_components/pill/pill";
 import { Model } from "@/types/models";
+import Image from "next/image";
 import { notFound } from "next/navigation";
+import styles from "./page.module.css";
+import Link from "next/link";
 
 type ModelPageParams = {
   params: Promise<{
@@ -29,9 +34,55 @@ export default async function Page({ params }: ModelPageParams) {
 
 const ModelPage = ({ model }: { model: Model }) => {
   return (
-    <DefaultContainer>
-      <PageHeading>{model.name}</PageHeading>
-      <p>{model.description}</p>
+    <DefaultContainer className={styles.modelPage}>
+      <ModelImage model={model} />
+      <ModelInfo model={model} />
     </DefaultContainer>
+  );
+};
+
+const ModelImage = ({ model }: { model: Model }) => {
+  return (
+    <Image
+      src={model.image}
+      alt={model.name}
+      width={300}
+      height={300}
+      className={styles.modelImage}
+    />
+  );
+};
+
+const ModelInfo = ({ model }: { model: Model }) => {
+  return (
+    <div className={styles.modelInfo}>
+      <Likes likes={model.likes} size="2.4rem" />
+      <PageHeading className={styles.modelName} fontSize="small">
+        {model.name}
+      </PageHeading>
+      <Pill>{model.category}</Pill>
+      <p>{model.description}</p>
+      <DateAdded model={model} />
+      <BackLink />
+    </div>
+  );
+};
+
+const DateAdded = ({ model }: { model: Model }) => {
+  const date = new Date(model.dateAdded);
+  const formattedDate = date.toLocaleDateString("en-US", {
+    year: "numeric",
+    month: "long",
+    day: "numeric",
+  });
+
+  return <p className={styles.dateAdded}>Added on {formattedDate}</p>;
+};
+
+const BackLink = () => {
+  return (
+    <Link href="/3d-models" className={styles.backLink}>
+      Back to overview
+    </Link>
   );
 };
