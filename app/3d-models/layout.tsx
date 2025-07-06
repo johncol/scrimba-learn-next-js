23ed19845c336bbb355d66005426ef3bd96723c8
johncol <john.19col@gmail.com>
2025-07-06 16:10:27 -0700

    Make CategoriesNavigation client component and use usePathname to style the active category menu item

--- app/3d-models/layout.tsx
+++ app/3d-models/layout.tsx
@@ -3,11 +3,13 @@ import { DefaultContainer } from "../_components/container/default-container";
 
 import styles from "./layout.module.css";
 import { CategoriesNavigation } from "../_components/categories-navigation/categories-navigation";
+import { getAllCategories } from "../_api/categories/getAllCategories";
 
-export default function Layout({ children }: PropsWithChildren) {
+export default async function Layout({ children }: PropsWithChildren) {
+  const categories = await getAllCategories();
   return (
     <DefaultContainer className={styles.layout}>
-      <CategoriesNavigation />
+      <CategoriesNavigation categories={categories} />
       <div>{children}</div>
     </DefaultContainer>
   );
--- app/_components/categories-navigation/categories-navigation.tsx
+++ app/_components/categories-navigation/categories-navigation.tsx
@@ -1,11 +1,17 @@
-import { getAllCategories } from "@/app/_api/categories/getAllCategories";
+"use client";
+
 import Link from "next/link";
 
+import { Category } from "@/types/models";
+import { usePathname } from "next/navigation";
 import styles from "./categories-navigation.module.css";
 
-export const CategoriesNavigation = async () => {
-  const categories = await getAllCategories();
+type Props = {
+  categories: Category[];
+};
 
+export const CategoriesNavigation = ({ categories }: Props) => {
+  const pathname = usePathname();
   return (
     <ul className={styles.categoriesNavigation}>
       <li>
@@ -13,11 +19,24 @@ export const CategoriesNavigation = async () => {
       </li>
       {categories.map((category) => (
         <li key={category.slug}>
-          <Link href={`/3d-models/categories/${category.slug}`}>
-            {category.displayName}
-          </Link>
+          <CategoryLink category={category} pathname={pathname} />
         </li>
       ))}
     </ul>
   );
-}
+};
+
+type CategoryLinkProps = {
+  category: Category;
+  pathname: string;
+};
+
+const CategoryLink = ({ category, pathname }: CategoryLinkProps) => {
+  const linkPath = `/3d-models/categories/${category.slug}`;
+  const isActive = pathname === linkPath;
+  return (
+    <Link href={linkPath} className={isActive ? styles.active : ""}>
+      {category.displayName}
+    </Link>
+  );
+};
