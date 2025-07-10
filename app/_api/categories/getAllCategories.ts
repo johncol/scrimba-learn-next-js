3822bcbba64e9b82053f1db76182ee064f9eb0f1
johncol <john.19col@gmail.com>
2025-07-09 23:42:30 -0700

    Simplify queries by using queryDB function generic parameter T

--- app/_api/categories/getAllCategories.ts
+++ app/_api/categories/getAllCategories.ts
@@ -2,12 +2,9 @@ import type { Category } from "@/types/models";
 import { queryDB } from "../queryDB";
 
 export const getAllCategories = async (): Promise<Category[]> => {
-  return queryDB(async (prisma) => {
-    const categories = await prisma.category.findMany({
-      orderBy: {
-        displayName: "asc",
-      },
-    });
-    return categories as Category[];
-  });
+  return queryDB<Category[]>((prisma) =>
+    prisma.category.findMany({
+      orderBy: { displayName: "asc" },
+    })
+  );
 };
--- app/_api/categories/getCategoryBySlug.ts
+++ app/_api/categories/getCategoryBySlug.ts
@@ -1,11 +1,8 @@
 import type { Category } from "@/types/models";
 import { queryDB } from "../queryDB";
 
-export const getCategoryBySlug = (slug: string): Promise<Category> => {
-  return queryDB(async (prisma) => {
-    const category = await prisma.category.findUnique({
-      where: { slug },
-    });
-    return category as Category;
-  });
+export const getCategoryBySlug = (slug: string): Promise<Category | null> => {
+  return queryDB<Category | null>((prisma) =>
+    prisma.category.findUnique({ where: { slug } })
+  );
 };
